ca086f81e37159632892070ed28c70e72651f8ae
yavgni1212 <yavgni1212@gmail.com>
2021-10-25 12:57:37 +0300

    finished

--- src/Favorites/Favorites.js
+++ src/Favorites/Favorites.js
@@ -7,42 +7,13 @@ import { Layout } from "antd";
 import { Link } from "react-router-dom";
 import { FavoritesSearchDay } from "./FavoritesSearchDay";
 
-const { Header, Content, Footer } = Layout;
-
 export const Favorites = () => {
-
-  const [weatherOneDayInfo, setWeatherOneDayInfo] = useState();
   const [favourites, useFavourites] = useState(["moscow", "haifa", "elat"]);
-
-
-const citysArr = ["moscow", "haifa", "elat"];
-const arr=<FavoritesSearchDay citysArr={favourites}/>
-  console.log(arr);
-  
-
   return (
-    <div>
-      <div className={styles.searchWraper}></div>
-
-      <div
-        className="site-layout-background"
-        style={{ padding: 24, minHeight: 380 }}
-      >
-        <div className={styles.main}>
-          {!!weatherOneDayInfo &&
-            weatherOneDayInfo.map((i, index) => (
-              <div className={styles.day} key={index}>
-                <WeatherDay
-                  oneDay={true}
-                  imperial={i.imperial}
-                  weatherType={i.weatherType}
-                  weatherKey={i.weatherKey}
-                  dayOfWeek={i.dayOfWeek}
-                />
-              </div>
-            ))}
-        </div>
-      </div>
+    <div className={styles.main}>
+      {favourites.map((i, index) => (
+        <FavoritesSearchDay city={i} />
+      ))}
     </div>
   );
 };
--- src/Favorites/FavoritesSearchDay.js
+++ src/Favorites/FavoritesSearchDay.js
@@ -1,65 +1,56 @@
-import React from 'react'
+import React from "react";
 import { useEffect, useState } from "react";
-import { fetchSingleCity } from '../mockApi';
+import { fetchSingleCity } from "../mockApi";
+import { WeatherDay } from "../WeatherDay/WeatherDay";
+import styles from "./favorites.module.css";
 
+export const FavoritesSearchDay = ({ city }) => {
+  const [weatherOneDayInfo, setWeatherOneDayInfo] = useState([]);
 
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
+  const padNum = (num) => {
+    const stringNum = num + "";
+    const stringLen = stringNum.length;
 
-
-export const FavoritesSearchDay = ({citysArr}) => {
-       console.log(citysArr);
- 
-    const [weatherInfo, setWeatherInfo] = useState([]); 
-    const daysOfWeek = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-      ];
-
-    const padNum = (num) => {
-        const stringNum = num + "";
-        const stringLen = stringNum.length;
-    
-        if (stringLen === 1) {
-          return "0" + stringNum;
-        } else {
-          return stringNum;
-        }
-      };
-
-
-      useEffect(() => {
-           console.log("weatherOneDayInfo",weatherInfo)
-        if (weatherInfo.length < citysArr.length){
-            citysArr.map((i, index) => {
-              const oneDaysData = fetchSingleCity(i);
-              oneDaysData.then((res) => {
-                console.log(res);
-      
-                setWeatherInfo((weatherOneDayInfo) => [
-                  ...weatherOneDayInfo,
-                  {
-                    imperial: res[0].Temperature.Imperial.Value,
-                    // max: res.DailyForecasts[0].Temperature.Maximum.Value,
-                    weatherType: res[0].WeatherText,
-                    weatherKey: padNum(res[0].WeatherIcon),
-                    dayOfWeek:
-                      daysOfWeek[new Date(res[0].LocalObservationDateTime).getDay()],
-                  },
-                ]);
-              });
-            });
+    if (stringLen === 1) {
+      return "0" + stringNum;
+    } else {
+      return stringNum;
+    }
+  };
+  useEffect(() => {
+    const oneCity = fetchSingleCity(city);
+    oneCity.then((res) => {
+      console.log(res);
+      setWeatherOneDayInfo(
+        <WeatherDay
+          oneDay={true}
+          imperial={res[0].Temperature.Imperial.Value}
+          weatherType={res[0].WeatherText}
+          weatherKey={padNum(res[0].WeatherIcon)}
+          dayOfWeek={
+            daysOfWeek[new Date(res[0].LocalObservationDateTime).getDay()]
           }
-      }, []);
+        />
+      );
+    });
+  }, []);
 
-
-    return (
-        <div>
-            {weatherInfo}
-        </div>
-    )
-}
+  return (
+    <div
+      className="site-layout-background"
+      style={{ padding: 24, minHeight: 380 }}
+    >
+      {<div className={styles.day}>{weatherOneDayInfo}</div>}
+    </div>
+  );
+};
