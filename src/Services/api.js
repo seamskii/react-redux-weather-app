133416088eec5a597863b5d04c39f52ec2187ec0
yavgni1212 <yavgni1212@gmail.com>
2021-11-02 18:31:31 +0200

    https

--- src/Services/api.js
+++ src/Services/api.js
@@ -2,13 +2,13 @@ export const Key = "GpnxUsR80XXpnMzO5HIp92rg1YMuxaPI";
 
 const fetchWeekData = (currentLocationKey) => {
   const fiveDaysData = fetch(
-    `http://dataservice.accuweather.com/forecasts/v1/daily/5day/locationKey=${currentLocationKey}_PC?apikey=%09${Key}`
+    `https://dataservice.accuweather.com/forecasts/v1/daily/5day/locationKey=${currentLocationKey}_PC?apikey=%09${Key}`
   );
   return fiveDaysData;
 };
 const fetchCity = (citySearch) => {
   const CityData = fetch(
-    `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=%09${Key}&q=${citySearch}`
+    `https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=%09${Key}&q=${citySearch}`
   );
   return CityData;
 };
@@ -20,7 +20,7 @@ const fetchSingleCity = (city) => {
 };
 const fetchGeoposition = (latitude, longitude) => {
   const GeopositionData = fetch(
-    `http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${Key}&q=${latitude}%2C${longitude}`
+    `https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${Key}&q=${latitude}%2C${longitude}`
   );
   return GeopositionData;
 };
