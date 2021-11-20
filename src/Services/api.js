4e46510ea6ef6d2310c538254d350317db69f72f
yavgni1212 <yavgni1212@gmail.com>
2021-11-20 10:13:40 +0200

    try 3

--- src/Services/api.js
+++ src/Services/api.js
@@ -27,3 +27,32 @@ const fetchGeoposition = (latitude, longitude) => {
 };
 
 export { fetchWeekData, fetchCity, fetchSingleCity, fetchGeoposition };
+// export const Key = process.env.REACT_APP_MY_API_KEY;
+
+
+// const fetchWeekData = (currentLocationKey) => {
+//   const fiveDaysData = fetch(
+//     `https://dataservice.accuweather.com/forecasts/v1/daily/5day/locationKey=${currentLocationKey}_PC?apikey=%09${Key}`
+//   );
+//   return fiveDaysData;
+// };
+// const fetchCity = (citySearch) => {
+//   const CityData = fetch(
+//     `https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=%09${Key}&q=${citySearch}`
+//   );
+//   return CityData;
+// };
+// const fetchSingleCity = (city) => {
+//   const oneCity = fetch(
+//     `https://dataservice.accuweather.com/currentconditions/v1/locationKey=${city}_PC?apikey=%09${Key}`
+//   );
+//   return oneCity;
+// };
+// const fetchGeoposition = (latitude, longitude) => {
+//   const GeopositionData = fetch(
+//     `https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${Key}&q=${latitude}%2C${longitude}`
+//   );
+//   return GeopositionData;
+// };
+
+// export { fetchWeekData, fetchCity, fetchSingleCity, fetchGeoposition };
